dbc2409714573fe453b057868c8d6515051bd69a
derybakti <derybakti09@gmail.com>
2022-07-14 12:22:48 +0700

    web portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed = new Typed(".typing", {
+    strings:["Web Designer", "Web Developer", "Progammer", "Cyber Securty", "Game Developer", "Machine Learning Developer" ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
